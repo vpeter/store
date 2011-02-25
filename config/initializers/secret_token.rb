54c3b17d79909e3c4dd63ac9f490d70fba0403f0
vpeter <vagnerpeter01@gmail.com>
2011-02-25 16:33:43 +0100

    alapprojekt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '0fad10c98209f121dcc03a1b52b2dc3d917e06e8a4a0b9be17a8be38ed526433cac2b16b9ea076c686cdbc8dfa644946a3a20610e68c1127e69bfaf763863f2e'
